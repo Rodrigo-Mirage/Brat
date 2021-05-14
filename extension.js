ea1c9c7d7490cb67a67c2c9353309797f0be0025
Mirage <mirage4d@hotmail.com>
2021-05-14 17:43:24 -0300

    login twitch

--- extension.js
+++ extension.js
@@ -5,16 +5,12 @@ const {OBSUtility} = require('nodecg-utility-obs');
 module.exports = function (nodecg) {
 
     const obs = new OBSUtility(nodecg, {namespace: 'obs'});
-    const secondaryOBS = new OBSUtility(nodecg, {namespace: 'secondaryOBS'});
-
     obs.replicants.websocket.on('change', () => {/* ... */});
     obs.replicants.programScene.on('change', () => {/* ... */});
     obs.replicants.previewScene.on('change', () => {/* ... */});
     obs.replicants.sceneList.on('change', () => {/* ... */});
     obs.replicants.transitioning.on('change', () => {/* ... */});
     obs.replicants.studioMode.on('change', () => {/* ... */});
-
-    
 
 }
 
